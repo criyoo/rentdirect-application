44353da4324c144b57f14df8700c1768d4288395
Christian Aluya <criyo.career@gmail.com>
2026-07-28 11:58:02 +0100

    update api settings & update payout delay time

--- api/config/settings/test.py
+++ api/config/settings/test.py
@@ -33,6 +33,7 @@ MIDDLEWARE = [  # noqa: F405
 
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+VERIFICATION_SERVICE = "prembly"
 PAYMENT_QUEUE_BACKEND = "sync"
 PAYMENT_QUEUE_REDIS_URL = ""
 PAYMENT_QUEUE_URL = ""
